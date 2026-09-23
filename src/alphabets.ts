c8b5999d8e137ee16d121067d17b6fedbaf97fe8
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2026-09-24 00:07:07 +0300

    Inline nanoid-dictionary alphabets to drop the dependency
    The preset dropdown only needed a handful of static character sets, so
    keeping a runtime dependency for them was unnecessary supply-chain
    surface. Inline the sets in src/alphabets.ts with attribution to
    nanoid-dictionary (MIT) and source comments, and remove the package.

--- src/alphabets.ts
+++ src/alphabets.ts
@@ -1,15 +1,21 @@
-import {
-  alphanumeric,
-  cookieSafe,
-  cookieUnsafe,
-  hexadecimalLowercase,
-  hexadecimalUppercase,
-  lowercase,
-  nolookalikes,
-  numbers,
-  uppercase,
-} from 'nanoid-dictionary';
 import { ALPHABET } from './defaults';
+
+// Character sets from nanoid-dictionary (MIT License,
+// Copyright (c) 2018 Stanislav Lashmanov):
+// https://github.com/CyberAP/nanoid-dictionary
+export const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+export const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+export const numbers = '0123456789';
+export const alphanumeric = numbers + lowercase + uppercase;
+export const hexadecimalUppercase = numbers + 'ABCDEF';
+export const hexadecimalLowercase = numbers + 'abcdef';
+
+export const nolookalikes = '346789ABCDEFGHJKLMNPQRTUVWXYabcdefghijkmnpqrtwxyz';
+
+// conforms to https://www.rfc-editor.org/rfc/rfc6265.txt
+export const cookieSafe = alphanumeric + "!#$%&'*+-.^_`|~";
+// actually works in browsers https://stackoverflow.com/a/1969339
+export const cookieUnsafe = alphanumeric + "!#$%&'()*+-./:<=>?@[]^_`{|}~";
 
 export interface AlphabetPreset {
   id: string;
